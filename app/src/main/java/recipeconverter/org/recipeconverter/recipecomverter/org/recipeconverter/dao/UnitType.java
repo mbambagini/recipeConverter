c4e4d9aed76596501e201737f9d4af3c15feb687
Mario <mario.bambagini@gmail.com>
2015-10-08 16:34:51 +0200

    Create UnitType.java
    added new enum type for measurement units

---
+++ app/src/main/java/recipeconverter/org/recipeconverter/recipecomverter/org/recipeconverter/dao/UnitType.java
@@ -0,0 +1,12 @@
+package recipeconverter.org.recipeconverter.recipecomverter.org.recipeconverter.dao;
+
+public enum UnitType {
+    //WEIGHT
+    UNIT_OUNCE,
+    UNIT_POUND,
+    UNIT_GRAM,
+    UNIT_KILOGRAM,
+    //CAPACITY
+    UNIT_GALLON,
+    UNIT_LITRE
+}
