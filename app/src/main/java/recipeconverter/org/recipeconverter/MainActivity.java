81c46dba68cf7356f9439150a7ada815b7f4fa73
Mario Bambagini <mario.bambagini@gmail.com>
2015-10-10 12:01:18 +0200

    add draft of the first activity

--- app/src/main/java/recipeconverter/org/recipeconverter/MainActivity.java
+++ app/src/main/java/recipeconverter/org/recipeconverter/MainActivity.java
@@ -4,13 +4,50 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
+
+    static final int NO_CHOISE = 0;
+    static final int ONLY_PEOPLE = 1;
+    static final int ONLY_PAN = 2;
+    static final int PAN_PEOPLE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideComponents();
+        setPaggingVisibility(3);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.txtRecipePan);
+        List<String> list = new ArrayList<>();
+        list.add("Rectangle");
+        list.add("Square");
+        list.add("Circle");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                setPanShape(pos);
+                Toast.makeText(getApplicationContext(), "sel: " + pos, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
     }
 
     @Override
@@ -28,10 +65,68 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add_ingredients) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setPaggingVisibility(int numActive) {
+        findViewById(R.id.spacePadding1).setVisibility((numActive > 0) ? View.INVISIBLE : View.GONE);
+        findViewById(R.id.spacePadding2).setVisibility((numActive > 1) ? View.INVISIBLE : View.GONE);
+        findViewById(R.id.spacePadding3).setVisibility((numActive > 2) ? View.INVISIBLE : View.GONE);
+    }
+
+    private void hideComponents() {
+        findViewById(R.id.layoutHowManyPeople).setVisibility(View.GONE);
+        findViewById(R.id.layoutPan).setVisibility(View.GONE);
+        hidePanShapes();
+    }
+
+    private void hidePanShapes() {
+        findViewById(R.id.layoutShapeCircle).setVisibility(View.GONE);
+        findViewById(R.id.layoutShapeRect).setVisibility(View.GONE);
+        findViewById(R.id.layoutShapeSquare).setVisibility(View.GONE);
+    }
+
+    public void setPanShape(int id) {
+        hidePanShapes();
+        switch (id) {
+            case 0:
+                findViewById(R.id.layoutShapeRect).setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                findViewById(R.id.layoutShapeSquare).setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                findViewById(R.id.layoutShapeCircle).setVisibility(View.VISIBLE);
+                break;
+            default:
+                //hidePanShapes();
+                //findViewById(R.id.spacePadding4).setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void onClick(View v) {
+        setPaggingVisibility(0);
+        hidePanShapes();
+        switch (v.getId()) {
+            case R.id.btnPeople:
+                findViewById(R.id.layoutHowManyPeople).setVisibility(View.VISIBLE);
+                findViewById(R.id.layoutPan).setVisibility(View.GONE);
+                setPaggingVisibility(1);
+                break;
+            case R.id.btnPan:
+                findViewById(R.id.layoutHowManyPeople).setVisibility(View.GONE);
+                findViewById(R.id.layoutPan).setVisibility(View.VISIBLE);
+                setPaggingVisibility(1);
+                break;
+            case R.id.btnPeoplePan:
+                findViewById(R.id.layoutHowManyPeople).setVisibility(View.VISIBLE);
+                findViewById(R.id.layoutPan).setVisibility(View.VISIBLE);
+                setPaggingVisibility(0);
+                break;
+        }
     }
 }
