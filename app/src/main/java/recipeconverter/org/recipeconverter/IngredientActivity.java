c96496ee21b63865247ea1e87b81f72f7896df1b
Mario <mario.bambagini@gmail.com>
2015-10-12 14:37:28 +0200

    Update IngredientActivity.java
    added feature to add ingredients

--- app/src/main/java/recipeconverter/org/recipeconverter/IngredientActivity.java
+++ app/src/main/java/recipeconverter/org/recipeconverter/IngredientActivity.java
@@ -1,5 +1,8 @@
 package recipeconverter.org.recipeconverter;
 
+import recipeconverter.org.recipeconverter.exception.WrongInputs;
+import recipeconverter.org.recipeconverter.exception.IngredientAlreadyPresent;
+
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -26,6 +29,16 @@ public class IngredientActivity extends ActionBarActivity {
         setContentView(R.layout.activity_ingredient);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinnerIngredientUnit);
+        List<String> list = new ArrayList<>();
+        list.add("ounce");
+        list.add("pound");
+        list.add("gram");
+        list.add("kilogram");
+        list.add("gallon");
+        list.add("litre");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         ListView lst = (ListView) findViewById(R.id.lst_ingredients);
         ingredientList = new ArrayList<>();
@@ -33,19 +46,46 @@ public class IngredientActivity extends ActionBarActivity {
         lst.setAdapter(adapter);
     }
 
+    private IngredientEntry checkInputs () throws WrongInputs, IngredientAlreadyPresent {
+        IngredientEntry ingredient = new IngredientEntry();
+
+        String name = ((EditText)findViewById(R.id.txtIngredientName)).getText().toString();
+        if (name.compareTo("") == 0)
+            throw new WrongInputs();
+        for (IngredientEntry entry: ingredientList)
+            if (entry.getName().compareTo(name) == 0)
+                throw new IngredientAlreadyPresent();
+        ingredient.setName(editText);
+
+        double quantity = Double.parseDouble(((EditText)findViewById(R.id.txtIngredientQuantity)).getText().toString());
+        if (quantity <= 0.0)
+            throw new WrongInputs();
+        ingredient.setQuantity(quantity);
+
+        ingredient.setUnit(UnitType.fromInteger(((Spinner) findViewById(R.id.spinnerIngredientUnit)).getSelectedItemPosition() + 1));
+    }
+    
+    private void cleanInptuts () {
+        ((EditText)findViewById(R.id.txtIngredientName)).getText().clear();
+        ((EditText)findViewById(R.id.txtIngredientQuantity)).getText().clear();
+    }
+
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnAddIngredient:
-                EditText editText = (EditText) findViewById(R.id.txtIngredientName);
-                IngredientEntry ingredient = new IngredientEntry();
-                ingredient.setName(editText.getText().toString());
-                editText = (EditText) findViewById(R.id.txtIngredientQuantity);
-                ingredient.setQuantity(Double.parseDouble(editText.getText().toString()));
-                Spinner spinner = (Spinner) findViewById(R.id.spinnerIngredientUnit);
-                ingredient.setUnit(UnitType.fromInteger(spinner.getSelectedItemPosition()));
+        case R.id.btnAddIngredient:
+            try {
+                IngredientEntry ingredient = checkInputs();
                 ingredientList.add(ingredient);
                 adapter.notifyDataSetChanged();
-                break;
+                cleanInptuts();
+            } catch (WrongInputs e) {
+                Toast.makeText(getApplicationContext(), "Set all inputs correctly", Toast.LENGTH_SHORT).show();
+                return;
+            } catch (IngredientAlreadyPresent e) {
+                Toast.makeText(getApplicationContext(), "Ingredient already present", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            break;
         }
     }
 
