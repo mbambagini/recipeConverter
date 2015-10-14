b44e099902358fcd50dc5affb0e0660caafa0a3a
Mario <mario.bambagini@gmail.com>
2015-10-14 09:56:50 +0200

    Update UnitType.java

--- app/src/main/java/recipeconverter/org/recipeconverter/dao/UnitType.java
+++ app/src/main/java/recipeconverter/org/recipeconverter/dao/UnitType.java
@@ -2,14 +2,15 @@ package recipeconverter.org.recipeconverter.dao;
 
 public enum UnitType {
     //WEIGHT
-    UNIT_NOT_VALID,
     UNIT_OUNCE,
     UNIT_POUND,
     UNIT_GRAM,
     UNIT_KILOGRAM,
     //CAPACITY
     UNIT_GALLON,
-    UNIT_LITRE;
+    UNIT_LITRE,
+    //others
+    UNIT_NOT_VALID;
 
     static public String toString(UnitType par) {
         switch (par) {
@@ -31,26 +32,25 @@ public enum UnitType {
 
     public static UnitType fromInteger (int par) {
         switch(par) {
-            case 1: return UNIT_OUNCE;
-            case 2: return UNIT_POUND;
-            case 3: return UNIT_GRAM;
-            case 4: return UNIT_KILOGRAM;
-            case 5: return UNIT_GALLON;
-            case 6: return UNIT_LITRE;
+            case 0: return UNIT_OUNCE;
+            case 1: return UNIT_POUND;
+            case 2: return UNIT_GRAM;
+            case 3: return UNIT_KILOGRAM;
+            case 4: return UNIT_GALLON;
+            case 5: return UNIT_LITRE;
         }
         return UNIT_NOT_VALID;
     }
 
     public static int toInteger (UnitType par) {
         switch(par) {
-            case UNIT_NOT_VALID: return 0;
-            case UNIT_OUNCE: return 1;
-            case UNIT_POUND: return 2;
-            case UNIT_GRAM: return 3;
-            case UNIT_KILOGRAM: return 4;
-            case UNIT_GALLON: return 5;
-            case UNIT_LITRE: return 6;
+            case UNIT_OUNCE: return 0;
+            case UNIT_POUND: return 1;
+            case UNIT_GRAM: return 2;
+            case UNIT_KILOGRAM: return 3;
+            case UNIT_GALLON: return 4;
+            case UNIT_LITRE: return 5;
         }
-        return 0;
+        return -1;
     }
 }
