b3ad8f8d3c4449177acb9217de50aae2cfbbdd31
Mario <mario.bambagini@gmail.com>
2015-10-15 10:11:00 +0200

    Update RecipeAdapter.java
    bug fixing

--- app/src/main/java/recipeconverter/org/recipeconverter/adapter/RecipeAdapter.java
+++ app/src/main/java/recipeconverter/org/recipeconverter/adapter/RecipeAdapter.java
@@ -26,7 +26,7 @@ public class RecipeAdapter extends ArrayAdapter<RecipeEntry> {
         this.context = context;
         recipes = values;
         if (typeFace == null)
-            typeFace = Typeface.createFromAsset(getAssets(), "fonts/JennaSue.ttf");
+            typeFace = Typeface.createFromAsset(context.getAssets(), "fonts/JennaSue.ttf");
     }
 
     @Override
